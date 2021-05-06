9a297089f8a45075bca32105db4037503b51d3a9
dengdejiangit <ddj+171121>
2021-05-06 14:48:41 +0800

    这是修改了一个语音的xxx

--- app/src/main/java/com/example/app_css/MainActivity.java
+++ app/src/main/java/com/example/app_css/MainActivity.java
@@ -12,7 +12,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("TAG", "onCreate: "+"哈哈哈 创建子分支，然后修改了");
+        Log.i("TAG", "onCreate: " + "哈哈哈 创建子分支，然后修改了");
+        Log.i("TAG", "onCreate: " + "哈哈哈 创建子分支，然后修改了");
+        Log.i("TAG", "onCreate: " + "哈哈哈 创建子分支，然后修改了");
+        Log.i("TAG", "onCreate: " + "哈哈哈 创建子分支，然后修改了" + "/////////////////////");
+        Log.i("TAG", "onCreate: " + "哈哈哈 创建子分支，然后修改了" + "/////////////////////" + "这是修改的内容");
+        Log.i("TAG", "onCreate: " + "哈哈哈 创建子分支，然后修改了" + "/////////////////////" + "这是修改的内容"+"这是就行ssssssssss修改了当前相机的bug");
+
 
     }
 }
